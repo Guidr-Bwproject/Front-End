e0e6f8e76d54a429d18e809a9d5d907b346edb2f
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-26 16:46:22 -0600

    fixed card styling, close to style guide

--- client/src/components/HomePageCards.js
+++ client/src/components/HomePageCards.js
@@ -4,10 +4,15 @@ import {Card, Icon, Image} from "semantic-ui-react";
 
 export default function HomePageCards ({name,image,species}){
   return(
-    <Card Style={{width: "60%", backgroundColor: "#f7f7f7", opacity: ".7", margin: "5%", borderRadius: "10px", boxShadow: "0px 5px 5px", }}>
-      <Card.Image>{image}</Card.Image>
-      <Card.Meta><span>guideName | date</span></Card.Meta>
-      <Card.Description><p>Muskellunge trout combtail gourami sea raven sole porbeagle shark freshwater hatchetfish spinefoot limia ghoul. Barbeled dragonfish sturgeon beardfish, mud catfish rockfish blue danio. Dogfish sabertooth; merluccid</p></Card.Description>
+    <Card style={{width: "60%", backgroundColor: "#f7f7f7", opacity: ".7", margin: "5%", borderRadius: "10px", boxShadow: "0px 5px 5px"}}>
+      <Image src={image} fluid />
+      <Card.Content style={{padding: "1% 5%"}}>
+        <Card.Header style={{textAlign: "left", fontSize: "2rem", fontWeight: "bold", color: "#314b11", fontFamily: "Roboto"}}>trip name {name}</Card.Header>
+        <Card.Meta style={{textAlign: "left", color: "#314b11"}}>guide name | date</Card.Meta>
+        <Card.Description style={{textAlign: "left", width: "80%", margin: "auto"}}>
+        Muskellunge trout combtail gourami sea raven sole porbeagle shark freshwater hatchetfish spinefoot limia ghoul. Barbeled dragonfish sturgeon beardfish, mud catfish rockfish blue danio. Dogfish sabertooth {species}
+        </Card.Description>
+      </Card.Content>
     </Card>
   )
 }
