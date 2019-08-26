63fb065efb82d98801843a30cfdb122e90cb5e6c
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-26 11:54:12 -0600

    added basic structure to HomePageCards

--- client/src/components/HomePageCards.js
+++ client/src/components/HomePageCards.js
@@ -1,1 +1,12 @@
 import React from "react";
+import {Card, Icon, Image} from "semantic-ui-react";
+
+export default function HomePageCards ({tripName, image, info}) {
+  return(
+    <Card
+      header={tripName}
+      image={image}
+      description={info}
+    />
+  )
+}
