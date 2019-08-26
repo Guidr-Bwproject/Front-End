3d4e29b3d7bfb7cd1ca31232482818ca832d08a7
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-26 15:35:53 -0600

    basic styling underway

--- client/src/components/HomePageCards.js
+++ client/src/components/HomePageCards.js
@@ -5,10 +5,10 @@ import {Card, Icon, Image} from "semantic-ui-react";
 export default function HomePageCards ({name,image,species}){
   return(
     <Card
-      style={{width: "60%", backgroundColor: "#f7f7f7", opacity: ".7", margin: "5%", borderRadius: "10px", boxShadow: "0px 5px 5px"}}
+      style={{width: "60%", backgroundColor: "#f7f7f7", opacity: ".7", margin: "5%", borderRadius: "10px", boxShadow: "0px 5px 5px", }}
       image={image}
       header={`this will be the trip name ${name}`}
-      description={`Muskellunge trout combtail gourami sea raven sole porbeagle shark freshwater hatchetfish spinefoot limia ghoul. Barbeled dragonfish sturgeon beardfish, mud catfish rockfish blue danio. Dogfish sabertooth; merluccid hake Mexican golden trout driftwood catfish dhufish murray cod amur pike noodlefish? Tripletail dwarf loach butterfly ray fierasfer dusky grouper. Cow shark spearfish chub herring; Oriental loach; yellowfin tuna anglerfish, thorny catfish snakehead longnose dace.${species}`}
+      description={`Muskellunge trout combtail gourami sea raven sole porbeagle shark freshwater hatchetfish spinefoot limia ghoul. Barbeled dragonfish sturgeon beardfish, mud catfish rockfish blue danio. Dogfish sabertooth; merluccid${species}`}
     />
   )
 }
