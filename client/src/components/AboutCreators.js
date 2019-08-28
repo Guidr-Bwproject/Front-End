21a1510d6c7398ba786198bcde42b26424c9ae2d
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-28 09:15:38 -0600

    added Wais profile pic

--- client/src/components/AboutCreators.js
+++ client/src/components/AboutCreators.js
@@ -6,6 +6,7 @@ import MichaelProfile from "../imgs/MichaelProfile.jpg";
 import MarquesProfile from "../imgs/MarquesProfile.jpg";
 import NicholasProfile from "../imgs/NicholasProfile.jpg";
 import MarkProfile from "../imgs/MarkProfile.png";
+import WaisProfile from "../imgs/WaisProfile.jpg";
 import PandaMailbox from "../imgs/PandaMailbox.JPG";
 import styled from "styled-components";
 
@@ -110,7 +111,7 @@ export default function AboutCreatorCard ({}){
 
     <div className="creatorCard">
     <Card className="creatorCardContainer">
-      <Image src={PandaMailbox} fluid className="creatorPic"/>
+      <Image src={WaisProfile} fluid className="creatorPic"/>
       <Card.Content>
         <Card.Header className="cardHeader">
           Wais Almakaleh
